67439578a93a75f2dde2f2bf6315c6b46ba88933
Williams Estrada <w.estrada.r@outlook.com>
2019-07-03 17:31:02 -0500

    Add sales/paymet controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   namespace :sales do
+    resources :payments, except: %i[new show]
     resources :bills, except: %i[new show]
     resources :deals, except: %i[new show]
     resources :test_drives, except: %i[new show]
