f7b972fcaf2cccd127558e1a1b33594b89c61624
Williams Estrada <w.estrada.r@outlook.com>
2019-07-03 10:39:10 -0500

    Add sales/test_drive controller

---
+++ app/controllers/sales/test_drives_controller.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module Sales
+  class TestDrivesController < ApplicationController
+    before_action :set_sales_test_drive, only: %i[edit update destroy]
+
+    def index
+      @sales_test_drives = Sales::TestDrive.all
+    end
+
+    def new
+      @sales_test_drive = Sales::TestDrive.new
+    end
+
+    def edit; end
+
+    def update
+      if @sales_test_drive.update(sales_test_drive_params)
+        redirect_to sales_test_drives_url, notice: 'Test drive was successfully updated.'
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @sales_test_drive.destroy
+      redirect_to sales_test_drives_url, notice: 'Test drive was successfully destroyed.'
+    end
+
+    private
+
+    def set_sales_test_drive
+      @sales_test_drive = Sales::TestDrive.find(params[:id])
+    end
+
+    def sales_test_drive_params
+      params.require(:sales_test_drive).permit(:client_name, :agent_name, :car, :date, :hour,
+                                               :license_number, :inital_miles, :final_miles)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   namespace :sales do
+    resources :test_drives, except: %i[new show]
     resources :cars, except: %i[new show]
     resources :addresses, except: %i[new show]
     resources :employees, except: %i[new show]
