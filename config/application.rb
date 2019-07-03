16a3ef8bfd2d32e129bc77ee951e07567279e8fb
Williams Estrada <w.estrada.r@outlook.com>
2019-07-03 18:37:24 -0500

    Change deafult generator options

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,11 @@ module CarDealer
     # the framework and any gems in your application.
 
     # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.system_tests false
+      g.jbuilder false
+      g.assets false
+      g.helper false
+    end
   end
 end
