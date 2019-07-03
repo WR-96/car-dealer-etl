d2771fca1641d99b0eba90ed4f08a49b21b0184b
Williams Estrada <w.estrada.r@outlook.com>
2019-07-03 18:54:13 -0500

    Use nested module/class definition

--- app/controllers/sales/clients_controller.rb
+++ app/controllers/sales/clients_controller.rb
@@ -1,38 +1,40 @@
 # frozen_string_literal: true
 
-class Sales::ClientsController < ApplicationController
-  before_action :set_sales_client, only: %i[edit update destroy]
+module Sales
+  class ClientsController < ApplicationController
+    before_action :set_sales_client, only: %i[edit update destroy]
 
-  def index
-    @sales_clients = Sales::Client.all
-  end
-
-  def new
-    @sales_client = Sales::Client.new
-  end
-
-  def edit; end
-
-  def update
-    if @sales_client.update(sales_client_params)
-      redirect_to sales_clients_url, notice: 'Client was successfully updated.'
-    else
-      render :edit
+    def index
+      @sales_clients = Sales::Client.all
     end
-  end
 
-  def destroy
-    @sales_client.destroy
-    redirect_to sales_clients_url, notice: 'Client was successfully destroyed.'
-  end
+    def new
+      @sales_client = Sales::Client.new
+    end
 
-  private
+    def edit; end
 
-  def set_sales_client
-    @sales_client = Sales::Client.find(params[:id])
-  end
+    def update
+      if @sales_client.update(sales_client_params)
+        redirect_to sales_clients_url, notice: 'Client was successfully updated.'
+      else
+        render :edit
+      end
+    end
 
-  def sales_client_params
-    params.require(:sales_client).permit(:name, :phone, :cellphone, :email, :rfc)
+    def destroy
+      @sales_client.destroy
+      redirect_to sales_clients_url, notice: 'Client was successfully destroyed.'
+    end
+
+    private
+
+    def set_sales_client
+      @sales_client = Sales::Client.find(params[:id])
+    end
+
+    def sales_client_params
+      params.require(:sales_client).permit(:name, :phone, :cellphone, :email, :rfc)
+    end
   end
 end
--- app/controllers/sales/payments_controller.rb
+++ app/controllers/sales/payments_controller.rb
@@ -1,39 +1,41 @@
 # frozen_string_literal: true
 
-class Sales::PaymentsController < ApplicationController
-  before_action :set_sales_payment, only: %i[edit update destroy]
+module Sales
+  class PaymentsController < ApplicationController
+    before_action :set_sales_payment, only: %i[edit update destroy]
 
-  def index
-    @sales_payments = Sales::Payment.all
-  end
-
-  def new
-    @sales_payment = Sales::Payment.new
-  end
-
-  def edit; end
-
-  def update
-    if @sales_payment.update(sales_payment_params)
-      redirect_to sales_payments_url, notice: 'Payment was successfully updated.'
-    else
-      render :edit
+    def index
+      @sales_payments = Sales::Payment.all
     end
-  end
 
-  def destroy
-    @sales_payment.destroy
-    redirect_to sales_payments_url, notice: 'Payment was successfully destroyed.'
-  end
+    def new
+      @sales_payment = Sales::Payment.new
+    end
 
-  private
+    def edit; end
 
-  def set_sales_payment
-    @sales_payment = Sales::Payment.find(params[:id])
-  end
+    def update
+      if @sales_payment.update(sales_payment_params)
+        redirect_to sales_payments_url, notice: 'Payment was successfully updated.'
+      else
+        render :edit
+      end
+    end
 
-  def sales_payment_params
-    params.require(:sales_payment).permit(:client_name, :deal_number, :status, :due_date,
-                                          :payment_date, :amount)
+    def destroy
+      @sales_payment.destroy
+      redirect_to sales_payments_url, notice: 'Payment was successfully destroyed.'
+    end
+
+    private
+
+    def set_sales_payment
+      @sales_payment = Sales::Payment.find(params[:id])
+    end
+
+    def sales_payment_params
+      params.require(:sales_payment).permit(:client_name, :deal_number, :status, :due_date,
+                                            :payment_date, :amount)
+    end
   end
 end
