48e42cbdc96f4f51f67614d66efb01f098b46ed6
Williams Estrada <w.estrada.r@outlook.com>
2019-07-26 18:46:01 -0500

    Validate sales_addresses form

--- app/models/sales/address.rb
+++ app/models/sales/address.rb
@@ -2,5 +2,9 @@
 
 module Sales
   class Address < ApplicationRecord
+    validates :client_name, :address_line, :city, :state, :country, :zip_code, presence: true
+    validates :client_name, length: { maximum: 50 }
+    validates :address_line, :city, :state, :country, length: { maximum: 100 }
+    validates :zip_code, numericality: { only_integer: true }, length: { is: 5}
   end
 end
