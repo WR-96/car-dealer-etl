1598dd4aa06f58ed4579378a100826a82b030aa8
Williams Estrada <w.estrada.r@outlook.com>
2019-07-26 16:09:40 -0500

    Add helper to validate model attributes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,22 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
+  def td_for(model, attr)
+    tooltip_title = ''
+
+    model.valid?
+    model.errors[attr].each do |msg|
+      tooltip_title += "#{msg}\n"
+    end
+
+    attr_hash = { class: ('alert-danger' unless model.valid_attribute?(attr)),
+                  data: { toggle: 'tooltip', placement: 'top', html: 'true' },
+                  title: tooltip_title }
+
+    haml_tag :td, attr_hash do
+      haml_concat model[attr]
+    end
+
+  end
 end
