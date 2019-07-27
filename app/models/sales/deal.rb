484e3edb4c5e5ce1d612a814e5575e0c3d8c6356
Williams Estrada <w.estrada.r@outlook.com>
2019-07-26 21:41:36 -0500

    Validates sales_deal form

--- app/models/sales/deal.rb
+++ app/models/sales/deal.rb
@@ -2,5 +2,12 @@
 
 module Sales
   class Deal < ApplicationRecord
+    attribute :date, :date
+    validates :client_name, :agent_name, :car, :price, :date, :monthly_payment, :payment_due_day,
+              presence: true
+    validates :client_name, :agent_name, :car, length: { maximum: 50 }
+    validates :price, :monthly_payment, numericality: { greater_than: 0 }
+    validates :date, date: true
+    validates :payment_due_day, numericality: { greater_than: 0, less_than: 26 }
   end
 end
