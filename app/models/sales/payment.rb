06ca35589b3968e8ea1046ccebd0a721da52577a
Williams Estrada <w.estrada.r@outlook.com>
2019-07-26 22:50:56 -0500

    Validates sales_payment form

--- app/models/sales/payment.rb
+++ app/models/sales/payment.rb
@@ -2,5 +2,13 @@
 
 module Sales
   class Payment < ApplicationRecord
+    attribute :due_date, :date
+    attribute :payment_date, :date
+    validates :client_name, :status, :due_date, :payment_date, :amount, presence: true
+    validates :client_name, length: { maximum: 50 }
+    validates :status, inclusion: { in: %w[payed paying cancelled],
+                                    message: "%{value} is not a valid status" }
+    validates :due_date, :payment_date, date: true
+    validates :amount, numericality: { greater_than: 0 }
   end
 end
