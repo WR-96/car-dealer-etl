d293b7cee0d334efe2a7ba6391ec27947f0725e9
Williams Estrada <w.estrada.r@outlook.com>
2019-07-26 20:55:07 -0500

    Validates sales_test_drive form

--- app/models/sales/test_drive.rb
+++ app/models/sales/test_drive.rb
@@ -2,5 +2,17 @@
 
 module Sales
   class TestDrive < ApplicationRecord
+    attribute :date, :date
+    validates :client_name, :agent_name, :car, :date, :hour, :license_number, :inital_miles,
+              :final_miles, presence: true
+    validates :client_name, :agent_name, :car, length: { maximum: 50 }
+    validates :date, date: true
+    validates :license_number, numericality: { only_integer: true }, length: { is: 8 }
+    validates :inital_miles, :final_miles, numericality: { only_integer: true }
+    validates :final_miles, numericality: { greater_than: :inital_miles },
+              if: Proc.new { |p| p.inital_miles.is_a? Integer }
+    validates :hour, format: { with: /\A(2[0-3]|[01]?\d):[0-5]\d\z/,
+                               message: 'needs to be in 24hr format "HH:MM"' }
+
   end
 end
