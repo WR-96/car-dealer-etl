1b2f5d65b3400a474b692c3828dd5ac8196b131c
Williams Estrada <w.estrada.r@outlook.com>
2019-07-26 23:11:53 -0500

    Validate sales_client form

--- app/models/sales/client.rb
+++ app/models/sales/client.rb
@@ -2,5 +2,13 @@
 
 module Sales
   class Client < ApplicationRecord
+    validates :name, :phone, :cellphone, :email, :rfc, presence: true
+    validates :name, length: { maximum: 50 }, format: { without: /\d/,
+                                                        message: 'only allow letters' }
+    validates :phone, :cellphone, numericality: { only_integer: true }
+    validates :phone, length: { is: 7 }
+    validates :cellphone, length: { is: 10 }
+    validates :email, email: true
+    validates :rfc, format: { with: /[A-Z]{4}\d\d(0[1-9]|1[012])([0-2]\d|30|31)([A-Z]|\d){3}/}
   end
 end
