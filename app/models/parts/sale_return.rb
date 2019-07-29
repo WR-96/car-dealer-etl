9388a30752ad973697b5c6947252758ee88125bf
Williams Estrada <w.estrada.r@outlook.com>
2019-07-29 12:47:57 -0500

    Validate parts/sales_return form

--- app/models/parts/sale_return.rb
+++ app/models/parts/sale_return.rb
@@ -2,5 +2,11 @@
 
 module Parts
   class SaleReturn < ApplicationRecord
+    attribute :date, :date
+    validates :client_name, :article, :date, :refound, :comment, presence: true
+    validates :client_name, length: { maximum: 50 }
+    validates :article, :comment, length: { maximum: 250 }
+    validates :date, date: true
+    validates :refound, numericality: { greater_than: 0 }
   end
 end
