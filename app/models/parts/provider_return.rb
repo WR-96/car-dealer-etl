37a9ec672b953cce9b29a5219389f5a382eebf5e
Williams Estrada <w.estrada.r@outlook.com>
2019-07-29 13:04:32 -0500

    Validate parts/provider_returns form

--- app/models/parts/provider_return.rb
+++ app/models/parts/provider_return.rb
@@ -2,5 +2,11 @@
 
 module Parts
   class ProviderReturn < ApplicationRecord
+    attribute :date, :date
+    validates :provider_name, :article, :date, :refound, :comment, presence: true
+    validates :provider_name, length: { maximum: 50 }
+    validates :article, :comment, length: { maximum: 250 }
+    validates :date, date: true
+    validates :refound, numericality: { greater_than: 0 }
   end
 end
