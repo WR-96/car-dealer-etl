9b33fa50c8aca3dfb689a10320eb7e65fbb3d532
Williams Estrada <w.estrada.r@outlook.com>
2019-07-29 12:29:32 -0500

    Validate parts/provider form

--- app/models/parts/provider.rb
+++ app/models/parts/provider.rb
@@ -2,5 +2,13 @@
 
 module Parts
   class Provider < ApplicationRecord
+    validates :tradename, :phone, :cellphone, :email, :car_maker, :piece, :price, presence: true
+    validates :tradename, length: { maximum: 150 }
+    validates :car_maker, length: { maximum: 50 }
+    validates :piece, length: { maximum: 250 }
+    validates :phone, length: { is: 7 }, numericality: { only_integer: true }
+    validates :cellphone, length: { is: 10 }, numericality: { only_integer: true }
+    validates :email, email: true
+    validates :price, numericality: { greater_than: 0 }
   end
 end
