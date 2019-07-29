11450077dc006cee681a1cc5c88ce9b23018ed21
Williams Estrada <w.estrada.r@outlook.com>
2019-07-29 10:56:57 -0500

    Validate parts_salse form

--- app/models/parts/sale.rb
+++ app/models/parts/sale.rb
@@ -2,5 +2,14 @@
 
 module Parts
   class Sale < ApplicationRecord
+    attribute :date, :date
+    validates :client_name, :employee_name, :date, :product, :price, :amount, 
+              :total, presence: true
+    validates :client_name, :employee_name, length: { maximum: 50 },
+              format: { without: /\d/, message: 'only allow letters' }
+    validates :date, date: true
+    validates :product, length: { maximum: 250 }
+    validates :price, :total, numericality: { greater_than: 0 }
+    validates :amount, numericality: { only_integer: true, greater_than: 0 }
   end
 end
