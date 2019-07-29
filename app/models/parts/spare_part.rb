7e0a14258c60c9861a0c9dee437805259216dbfd
Williams Estrada <w.estrada.r@outlook.com>
2019-07-29 12:02:03 -0500

    Validate parts/spare_parts form

--- app/models/parts/spare_part.rb
+++ app/models/parts/spare_part.rb
@@ -2,5 +2,11 @@
 
 module Parts
   class SparePart < ApplicationRecord
+    validates :description, :car_model, :car_maker, :price, :stock, :reorder, presence: true
+    validates :description, length: { maximum: 250 }
+    validates :car_model, :car_maker, length: { maximum: 50 }
+    validates :price, numericality: { greater_than: 0 }
+    validates :reorder, numericality: { only_integer: true, greater_than: 0 }
+    validates :stock, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   end
 end
