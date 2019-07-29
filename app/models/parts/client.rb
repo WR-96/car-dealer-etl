915a3a95f4a5bc0960aba30a4b4b70d12e21ebea
Williams Estrada <w.estrada.r@outlook.com>
2019-07-29 09:46:09 -0500

    Validate parts/client form

--- app/models/parts/client.rb
+++ app/models/parts/client.rb
@@ -2,5 +2,12 @@
 
 module Parts
   class Client < ApplicationRecord
+    validates :name, :phone, :cellphone, :email, :rfc, presence: true
+    validates :name, length: { maximum: 50 }, format: { without: /\d/,
+                                                         message: 'only allow letters'}
+    validates :phone, length: { is: 7 }, numericality: { only_integer: true }
+    validates :cellphone, length: { is: 10 }, numericality: { only_integer: true }
+    validates :email, email: true
+    validates :rfc, format: { with: /[A-Z]{4}\d\d(0[1-9]|1[012])([0-2]\d|30|31)([A-Z]|\d){3}/ }
   end
 end
