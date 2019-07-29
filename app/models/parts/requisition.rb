3aa2b2f7bf5b63aa14ccd48183871599b7401805
Williams Estrada <w.estrada.r@outlook.com>
2019-07-29 13:49:29 -0500

    Validate parts/requisition form

--- app/models/parts/requisition.rb
+++ app/models/parts/requisition.rb
@@ -2,5 +2,16 @@
 
 module Parts
   class Requisition < ApplicationRecord
+    attribute :date, :date
+    attribute :delivery_date, :date
+
+    validates :employee_name, :date, :status, :delivery_date, :article, :amount, presence: true
+    validates :employee_name, length: { maximum: 50 }, format: { without: /\d/,
+                                                                 message: 'only allow letters' }
+    validates :article, length: { maximum: 250 }
+    validates :date, :delivery_date, date: true
+    validates :status, inclusion: { in: %w[shipped pending delivered canceled],
+                                    message: "%{value} is not a valid status" }
+    validates :amount, numericality: { only_integer: true, greater_than: 0 }
   end
 end
