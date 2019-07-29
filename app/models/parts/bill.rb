855dee2a242e7f44de1af3c9cb8d54925630a45c
Williams Estrada <w.estrada.r@outlook.com>
2019-07-29 11:33:27 -0500

    Validate parst_bill form

--- app/models/parts/bill.rb
+++ app/models/parts/bill.rb
@@ -2,5 +2,14 @@
 
 module Parts
   class Bill < ApplicationRecord
+    attribute :date, :date
+    validates :client_name, :rfc, :date, :product, :price, :amount, :sub_total, :tax_percentage, 
+              :tax_amount, :total, presence: true
+    validates :client_name, length: { maximum: 50 }
+    validates :product, length: { maximum: 100 }
+    validates :rfc, format: { with: /[A-Z]{4}\d\d(0[1-9]|1[012])([0-2]\d|30|31)([A-Z]|\d){3}/ }
+    validates :date, date: true
+    validates :amount, :tax_percentage, numericality: { only_integer: true, greater_than: 0 }
+    validates :price, :sub_total, :tax_amount, :total, numericality: { greater_than: 0 } 
   end
 end
