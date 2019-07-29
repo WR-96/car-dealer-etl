9af1d869011cf027204a3c13b6134c06c9c66afe
Williams Estrada <w.estrada.r@outlook.com>
2019-07-29 13:39:43 -0500

    Validate parts/supplier_order form

--- app/models/parts/supplier_order.rb
+++ app/models/parts/supplier_order.rb
@@ -2,5 +2,18 @@
 
 module Parts
   class SupplierOrder < ApplicationRecord
+    attribute :date, :date
+    attribute :delivery_date, :date
+
+    validates :employee_name, :date, :supplier_tradename, :status, :delivery_date, :article,
+              :amount, :total, presence: true
+    validates :employee_name, length: { maximum: 50 }
+    validates :supplier_tradename, length: { maximum: 150 }
+    validates :article, length: { maximum: 250 }
+    validates :date, :delivery_date, date: true
+    validates :status, inclusion: { in: %w[shipped pending delivered canceled],
+                                    message: "%{value} is not a valid status" }
+    validates :amount, numericality: { only_integer: true, greater_than: 0 }
+    validates :total, numericality: { greater_than: 0 }
   end
 end
