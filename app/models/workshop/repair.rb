6067f2ce816bdbf001ae6b18c4a1813f279fed29
Williams Estrada <w.estrada.r@outlook.com>
2019-07-29 23:46:32 -0500

    Validate workshop/repair form

--- app/models/workshop/repair.rb
+++ app/models/workshop/repair.rb
@@ -2,5 +2,12 @@
 
 module Workshop
   class Repair < ApplicationRecord
+    attribute :date, :date
+    validates :date, :mechanic_name, :car, :description, :total, presence: true
+    validates :mechanic_name, :car, length: { maximum: 50 }
+    validates :mechanic_name, format: { without: /\d/, message: 'only allow letters' }
+    validates :date, date: true
+    validates :description, length: { maximum: 250 }
+    validates :total, numericality: { greater_than: 0 }
   end
 end
