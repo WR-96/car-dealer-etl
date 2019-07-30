1f1a389465297ecb339afa38e1e44a002dbbb655
Williams Estrada <w.estrada.r@outlook.com>
2019-07-30 07:25:23 -0500

    Modify sales/test_drive model to read inital_miles as integer

--- app/models/sales/test_drive.rb
+++ app/models/sales/test_drive.rb
@@ -3,6 +3,7 @@
 module Sales
   class TestDrive < ApplicationRecord
     attribute :date, :date
+    attribute :inital_miles, :integer
     validates :client_name, :agent_name, :car, :date, :hour, :license_number, :inital_miles,
               :final_miles, presence: true
     validates :client_name, :agent_name, :car, length: { maximum: 50 }
@@ -10,9 +11,8 @@ module Sales
                                          format: { without: /\d/, message: 'only allow letters' }
     validates :date, date: true
     validates :license_number, numericality: { only_integer: true }, length: { is: 8 }
-    validates :inital_miles, :final_miles, numericality: { only_integer: true }
-    validates :final_miles, numericality: { greater_than: :inital_miles },
-                            if: proc { |p| p.inital_miles.is_a? Integer }
+    validates :inital_miles, numericality: { only_integer: true, greater_than: 0}
+    validates :final_miles, numericality: { only_integer: true, greater_than: :inital_miles }
     validates :hour, format: { with: /\A(2[0-3]|[01]?\d):[0-5]\d\z/,
                                message: 'needs to be in 24hr format "HH:MM"' }
   end
