e6245ce1ea96a723da992eb866bd47a2bd0b4854
Williams Estrada <w.estrada.r@outlook.com>
2019-07-29 23:40:33 -0500

    Validate workshop/mechanics form

--- app/models/workshop/mechanic.rb
+++ app/models/workshop/mechanic.rb
@@ -2,5 +2,11 @@
 
 module Workshop
   class Mechanic < ApplicationRecord
+    validates :name, :phone, :cellphone, :email, presence: true
+    validates :name, length: { maximum: 50 }, format: { without: /\d/,
+                                                         message: 'only allow letters'}
+    validates :phone, length: { is: 7 }, numericality: { only_integer: true }
+    validates :cellphone, length: { is: 10 }, numericality: { only_integer: true }
+    validates :email, email: true
   end
 end
