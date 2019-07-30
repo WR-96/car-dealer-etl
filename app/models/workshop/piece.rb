eeb0cda479106362055b5fdedea66a4dd9d9b162
Williams Estrada <w.estrada.r@outlook.com>
2019-07-29 23:33:37 -0500

    Validate workshop/piece form

--- app/models/workshop/piece.rb
+++ app/models/workshop/piece.rb
@@ -2,5 +2,9 @@
 
 module Workshop
   class Piece < ApplicationRecord
+    validates :description, :maker, :car, :price, presence: true
+    validates :description, length: { maximum: 250 }
+    validates :maker, :car, length: { maximum: 50 }
+    validates :price, numericality: { greater_than: 0 }
   end
 end
