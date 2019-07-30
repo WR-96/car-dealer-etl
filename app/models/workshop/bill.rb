36c17f71c431fa2c4e4f2772028fce42d69845a6
Williams Estrada <w.estrada.r@outlook.com>
2019-07-29 23:50:18 -0500

    Validate workshop/bills form

--- app/models/workshop/bill.rb
+++ app/models/workshop/bill.rb
@@ -2,5 +2,15 @@
 
 module Workshop
   class Bill < ApplicationRecord
+    attribute :date, :date
+    validates :client_name, :rfc, :date, :service, :sub_total, :tax_percentage, 
+              :tax_amount, :total, presence: true
+    validates :client_name, length: { maximum: 50 }, format: { without: /\d/,
+                                                               message: 'only allow letters' }
+    validates :service, length: { maximum: 100 }
+    validates :rfc, format: { with: /[A-Z]{4}\d\d(0[1-9]|1[012])([0-2]\d|30|31)([A-Z]|\d){3}/ }
+    validates :date, date: true
+    validates :tax_percentage, numericality: { only_integer: true, greater_than: 0 }
+    validates :sub_total, :tax_amount, :total, numericality: { greater_than: 0 } 
   end
 end
